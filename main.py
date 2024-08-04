0d6243e634bd8d3d86975634dee2f9c598c461dd
Abhishek Yadav <148570859+AbhishekY9@users.noreply.github.com>
2024-08-04 17:35:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import streamlit as st
 from langchain_helper import get_few_shot_db_chain
-import sqlite3
-
 st.markdown(
     """
     <style>
@@ -20,16 +18,12 @@ question = st.text_input("Question: ")
 
 if question:
     try:
-        st.write("Initializing the chain...")
         chain = get_few_shot_db_chain()
-        st.write("Chain initialized successfully.")
-
-        st.write("Running the chain with the input question...")
         response = chain.run(question)
 
         st.header("Answer")
         st.write(response)
     except Exception as e:
-        st.error(f"An error occurred: {e}")
+        st.error("Please enter your question again.")
 else:
     st.info("Please enter a question.")
